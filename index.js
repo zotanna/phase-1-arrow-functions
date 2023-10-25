8ff69dc3f0792c568d03dc9891e71820b46c5cd3
denis <denniiskim@gmail.com>
2023-10-25 04:11:06 +0300

    Redone assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide(){
+    let result = 2000 / 100;
+    return result;
+  }
+  
+  divide();
+  
+  const square = (num) => num * num;
+  square(5);
+  
+  const add = (num1, num2) => num1 + num2;
+  add(12,5);
